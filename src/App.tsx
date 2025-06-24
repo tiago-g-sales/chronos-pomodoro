7dba52392cbf4c574e3e3f31acfe10ddae7a0348
Tiago Sales <tiagosales@outlook.com>
2025-06-24 13:54:46 -0300

    update project template

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,11 @@
 
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
+import { Home } from './pages/Home';
 
 import './styles/theme.css'
 import './styles/global.css'
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
 
 
 export function App() {
-  return (
-    <>
-    <Container>
-      <Logo/>
-    </Container>
-    <Container>
-      <Menu/>
-    </Container>
-    <Container>
-      <CountDown/>
-    </Container>
-    <Container>
-      <form className='form' action=""> 
-        <div className='formRow'>
-          <DefaultInput labelText='Task' id='meuInput' type='text' placeholder='Digite algo'/>
-        </div>
-        
-        <div className='formRow'>
-          <p>Lorem ipsum dolor sit amet.</p>
-        </div>   
 
-        <div className='formRow'>
-          <Cycles/>
-        </div>
-
-        <div className='formRow'>
-          < DefaultButton icon={< PlayCircleIcon />}/>
-          < DefaultButton icon={< StopCircleIcon/>} color='red'/>
-        </div>  
-      </form> 
-    </Container>
-
-    </>
-  );}
+  return <Home />
+  }
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,24 +1,61 @@
 
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react'
+import { HistoryIcon, HouseIcon, MoonIcon, SettingsIcon, SunIcon } from 'lucide-react'
 import styles from './styles.module.css'
+import { useState, useEffect } from 'react';
 
-
+type AvailableThemes = 'light' | 'dark';
 
 export function Menu( ) {
 
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storedTheme = localStorage.getItem('theme') as AvailableThemes || 'dark';
+    return storedTheme ;
+  });
+
+  const nextThemeIcon = {
+    light: <MoonIcon />,
+    dark: <SunIcon />
+
+  }
+
+
+  function handleThemeChange( 
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ){
+    event.preventDefault();
+
+    setTheme((prevTheme) => {
+      const newTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return newTheme;
+    }
+    );
+
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
   return (
     <nav className={styles.menu} >
-      <a className={styles.menuLink} href='#'>
+      <a className={styles.menuLink} href='#' aria-label='Ir para a Home' title='Ir para a Home'>
       <HouseIcon  />                                      
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a className={styles.menuLink} href='#' aria-label='Ver Histórico' title='Ver Histórico'>
       <HistoryIcon  />                                      
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a className={styles.menuLink} href='#' aria-label='Configurações' title='Configurações'>
       <SettingsIcon  />                                      
       </a>
-      <a className={styles.menuLink} href='#'>
-      <SunIcon  />                                      
+      <a 
+      className={styles.menuLink} 
+      href='#' 
+      aria-label='Mudar Tema' 
+      title='Mudar Tema' 
+      onClick= { handleThemeChange}
+      >
+      {nextThemeIcon[theme]}                                      
       </a>
     </nav>
   )
